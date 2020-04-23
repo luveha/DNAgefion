aa21dea849d0708bd804fefb4f34f28ec8f782dc
luveha <54841459+luveha@users.noreply.github.com>
2020-04-23 02:40:21 +0200

    Update main.py

--- main.py
+++ main.py
@@ -9,6 +9,29 @@ from BLAST import *
 from popUp import *
 from pyglet.window import Window
 
+#Removes all text in var entries
+def clear(*boxes):
+    for i in range(0, len(boxes)):
+        boxes[i].delete(first=0, last=len(boxes[i].get()))
+#Inserts Correct strings(popup + remove)
+def DNAmagic(inVar, outVar1, outVar2, outVar3):
+    inVarText = addSpace(removeSpace(str(inVar.get()).upper()))
+    if not set(inVarText).issubset(AllowedLetters):
+        z = str(inVar.get())
+        clear(inVar, outVar1, outVar2, outVar3)
+        inVar.insert(END, z)
+        popUp(ErrorText)
+    if set(inVarText).issubset(AllowedLetters):
+        clear(inVar, outVar1, outVar2, outVar3)
+        inVar.insert(END, inVarText)
+        Comp = CompString(inVarText)
+        outVar1.insert(END, Comp)
+        RnaSet = T2U(inVarText)
+        outVar2.insert(END, RnaSet)
+        Amino = Space2hyphen(aminoLoop(RnaSet, DNAcombinations, AminoAcids))
+        outVar3.insert(END, Amino)
+
+
 #TKinter GUI
 program = Tk()
 program.title('DNA program')
